bc4ece21022be248a45d1dc3c364726f5cf7d180
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-04-17 16:55:00 -0300

    Add colors to plot

--- illustrating_the_problem.r
+++ illustrating_the_problem.r
@@ -1,17 +1,26 @@
+require(viridis);
+
+cols = viridis(100);
+
+graphics.off();
+dev.new(width=8, height=8);
 
 mydist = function(x){
-	dgamma(x - 10, shape=5, scale=5);
+	dgamma(x - 30, shape=15, scale=0.8);
 }
 
 x = seq(0, 100, length=2000);
-plot(x, mydist(x), type="l");
+plot(x, mydist(x), type="l", lwd=3, col=2, ylab="density");
+abline(v=30, col=1, lwd=3, lty=2);
 
 # Now we find the closes gamma near that function
 difference = function(params){
 	sum(abs(dgamma(x, shape=params[1], scale=params[2]) - mydist(x)));
 }
 
-params = optim(c(5, 5), difference, method="L-BFGS", lower=c(0, 0))$par;
-print(params);
+#params = optim(c(15, 0.8), difference, method="L-BFGS", lower=c(0, 0))$par;
+#print(params);
+params = c(147.4261944, 0.2829535); # What the above optimization finds
 
-lines(x, dgamma(x, shape=params[1], scale=params[2]));
+lines(x, dgamma(x, shape=params[1], scale=params[2]), lwd=3, lty=3, col=4);
+
