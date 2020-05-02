0be6ef2089f94c80bc976418636d3423aeb16556
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-05-02 09:11:00 -0300

    Add plot illustrating the estimator locations

--- illustrating_the_problem.r
+++ illustrating_the_problem.r
@@ -98,3 +98,73 @@ img = image_read("formulation-fig2.png");
 img = image_convert(img, type="grayscale");
 dev.new();
 plot(img);
+
+
+dev.new(width=scale*12, height=scale*8);
+
+hist(data, freq=FALSE, border=F, col="#A0A0A070", xlim=c(0, 100), ylim=c(0, 0.045), axes=FALSE, xlab="", ylab="");
+axis(1, pos=0);
+axis(2, pos=0);
+title(xlab="x", ylab="density", line=1);
+
+lines(x, dgamma(x, shape=realParams[1], scale=realParams[2]), lwd=mylwd[1], lty=mylty[1], col=mycolors[1]);
+
+n = length(data);
+CV = sd(data) / mean(data);
+est1 = min(data);
+est2 = min(data) - abs(min(data)) * (CV / log10(n));
+est3 = min(data) - abs(min(data)) * (1 / n);
+est4 = min(data) - abs(min(data)) * 1 * sqrt(log(log(n)) / (2*n));
+est5 = min(data) - abs(min(data)) * 1 * sqrt(-log(0.05/2) / (2*n));
+
+y = 0.02
+lwd = 4
+cols = viridis(6)[1:5];
+mycex = 2;
+segments(est1, 0, y1=y, col=cols[1], lwd=lwd, lty="12");
+points(est1, y, cex=mycex, col=cols[1], pch=19);
+#text(est1, y+0.002, "est1");
+
+y = y + 0.003
+segments(est3, 0, y1=y, col=cols[2], lwd=lwd, lty="12");
+points(est3, y, cex=mycex, col=cols[2], pch=19);
+#text(est3, y+0.002, "est3");
+
+y = y + 0.003
+segments(est4, 0, y1=y, col=cols[3], lwd=lwd, lty="12");
+points(est4, y, cex=mycex, col=cols[3], pch=19);
+#text(est4, y+0.002, "est4");
+
+y = y + 0.003
+segments(est2, 0, y1=y, col=cols[4], lwd=lwd, lty="12");
+points(est2, y, cex=mycex, col=cols[4], pch=19);
+#text(est2, y+0.002, "est2");
+
+y = y + 0.003
+segments(est5, 0, y1=y, col=cols[5], lwd=lwd, lty="12");
+points(est5, y, cex=mycex, col=cols[5], pch=19);
+#text(est5, y+0.002, "est5");
+
+legends=list(
+	"sample\nmin",
+	expression(over(CV, log[10](n))),
+	expression(over(1,n)),
+	expression(sqrt(over(log(log(n)), 2*n))),
+	expression(sqrt(over(-log(nu/2), 2*n)))
+);
+legends = legends[c(1,3,4,2,5)];
+
+myx = rev(c(0.5, 25, 45, 70, 80)) + 5;
+for(i in 1:5){
+	legend(myx[i], 0.039, yjust=0, legend=legends[[i]], pch=19, col=cols[i], box.lwd=0, pt.cex=2);
+}
+
+savePlot("formulation-fig3.png");
+system("convert formulation-fig3.png -crop 624x316+40+72 formulation-fig3.png");
+
+img = image_read("formulation-fig3.png");
+img = image_convert(img, type="grayscale");
+dev.new();
+plot(img);
+
+
