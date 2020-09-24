87e4d508fc2a6448b5bc5e787f13449ebde2b945
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-09-24 15:30:00 -0300

    Adapt to new experiment

--- experiments/test_cauchy.r
+++ experiments/test_cauchy.r
@@ -27,20 +27,20 @@ for(N in ALL.N){
 
 		estim = list();
 		estim[[1]] = data$min;
-		estim[[2]] = data$min - abs(data$min) * abs(data$stddev / data$mean) / log10(N);
-		estim[[3]] = data$min - abs(data$min) * data$stddev / N;
-		estim[[4]] = data$min - abs(data$min) * data$stddev * sqrt(log(log(N)) / (2*N));
-		estim[[5]] = data$min - abs(data$min) * data$stddev * sqrt(-log(0.05/2) / (2*N));
+		#estim[[2]] = data$min - abs(data$min) * abs(data$stddev / data$mean) / log10(N);
+		estim[[2]] = data$min - abs(data$min) * data$stddev / N;
+		estim[[3]] = data$min - abs(data$min) * data$stddev * sqrt(log(log(N)) / (2*N));
+		estim[[4]] = data$min - abs(data$min) * data$stddev * sqrt(-log(0.05/2) / (2*N));
 
-		dist01 = rep(0, 5);
-		for(i in 1:5){
+		dist01 = rep(0, 4);
+		for(i in 1:4){
 			dist01[i] = pcauchy(estim[[i]]);
 		}
 
 		#print(rbind(estim, dist01));
 
-		lik = rep(0, 5);
-		for(i in 1:5){
+		lik = rep(0, 4);
+		for(i in 1:4){
 			samples = data$samples - estim[[i]];
 
 			lik.f = function(param){
@@ -78,25 +78,24 @@ for(N in ALL.N){
 	plotConf  = rbind(plotConf, delta[-1]);
 }
 
-colnames(df) = c("N", paste("dist", 1:5, sep=""), paste("lik", 1:5, sep=""), "likBase");
+colnames(df) = c("N", paste("dist", 1:4, sep=""), paste("lik", 1:4, sep=""), "likBase");
 #print(df);
 
 graphics.off();
 dev.new(width=0.8*8, height=0.8*5.4);
 
 #print(plotMeans);
-base = plotMeans[,11];
+base = plotMeans[,9];
 par(lwd=3, mar=c(3, 2, 0.2, 0.2));
 palette(qualitative_hcl(palette="Dark 3", n=5));
-plot(ALL.N, plotMeans[,6]   - base, type="l", ylim=c(-0.05, 1), col=1, log="x", xlab="", ylab="");
-lines(ALL.N, plotMeans[,7]  - base, col=2, lty=2);
-lines(ALL.N, plotMeans[,8]  - base, col=3, lty=3);
-lines(ALL.N, plotMeans[,9]  - base, col=4, lty=4);
-lines(ALL.N, plotMeans[,10] - base, col=5, lty=5);
+#plot(ALL.N, plotMeans[,5]   - base, type="l", ylim=c(-0.05, 1), col=1, log="x", xlab="", ylab="");
+#lines(ALL.N, plotMeans[,6]  - base, col=2, lty=2);
+#lines(ALL.N, plotMeans[,7]  - base, col=3, lty=3);
+#lines(ALL.N, plotMeans[,8]  - base, col=4, lty=4);
 
-title(xlab="sample size", line=2);
+#title(xlab="sample size", line=2);
 
-legend("bottom", c(expression(textstyle(min)), expression(c1), expression(c2), expression(c3), expression(c4)), col=1:5, lwd=3, lty=1:5, seg.len=4, bg="#FFFFFFBB");
+#legend("bottom", c(expression(textstyle(min)), expression(c1), expression(c2), expression(c3), expression(c4)), col=1:5, lwd=3, lty=1:5, seg.len=4, bg="#FFFFFFBB");
 #savePlot("test-exponential-likelihood.png");
 
 
@@ -110,13 +109,12 @@ plot(ALL.N, plotMeans[,1], type="l", ylim=c(0, 0.1), col=1, log="x", xlab="", yl
 lines(ALL.N, plotMeans[,2], col=2, lty=2);
 lines(ALL.N, plotMeans[,3], col=3, lty=3);
 lines(ALL.N, plotMeans[,4], col=4, lty=4);
-lines(ALL.N, plotMeans[,5], col=5, lty=5);
 
-for(i in 1:5){
+for(i in 1:4){
 	arrows(x0=ALL.N, y0=plotMeans[,i] - plotConf[,i], y1=plotMeans[,i] + plotConf[,i], angle=90, code=3, length=0.02);
 }
 
 title(xlab="sample size", line=2);
 title(ylab=expression(F(paste(phantom(i), widehat(c), phantom(i)))), line=2);
 
-legend("topright", c(expression(textstyle(min)), expression(c1), expression(c2), expression(c3), expression(c4)), col=1:5, lwd=3, lty=1:5, seg.len=4, bg="#FFFFFFBB");
+legend("topright", c(expression(textstyle(min)), expression(c2), expression(c3), expression(c4)), col=1:4, lwd=3, lty=1:4, seg.len=4, bg="#FFFFFFBB");
